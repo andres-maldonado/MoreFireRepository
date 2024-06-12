d3ed42067171b48d4ca9b07da6ffdc6b66b07e7d
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-06-12 14:55:43 +0900

    Added testing script via command line

---
+++ Assets/TestingScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// woah, this is a very interesting script!
+// hey!
+
+// i have now changed the contents of this script to have absolutely no important information!
+// muahahahah >:)
