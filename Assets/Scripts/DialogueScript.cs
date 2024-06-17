f8f72a39024c706e9ff38e8dba679e58d8df1574
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-06-17 10:35:47 +0900

    Began working on dialogue box script.

---
+++ Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this script will handle dialogue boxes
+
+public class DialogueScrip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
