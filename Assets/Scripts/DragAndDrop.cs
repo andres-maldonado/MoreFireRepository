3de8feeb9c473a833c9eceade8d6fb979b9d2524
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-06-17 15:56:59 +0900

    Testing branches

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    // woah, local branch
     void OnMouseDrag() {
         Vector3 mouse_position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z + Camera.main.nearClipPlane));
         mouse_position.z = 0f;
