92ca27e0aa353d9e6f30ee6d892e202ca2e01d92
omnairb <mobrian2004@gmail.com>
2024-06-17 23:42:06 +0900

    Made a simple script for the inventory

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public string name;
+
+    //stretch goal to add descriptions to the items
+    public string desc;
+    //public Sprite img;
+
+}
