f32a674c6fb9ef4c12d79af952002fb92ea55918
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-06-17 19:27:33 +0900

    Made first minigame test scene

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
-    // woah, local branch
+    // moves the object to the mouse location and stops its velocity
     void OnMouseDrag() {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
         Vector3 mouse_position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z + Camera.main.nearClipPlane));
         mouse_position.z = 0f;
+
+        rigidbody.velocity = Vector2.zero;
+        rigidbody.angularVelocity = 0f;
         transform.position = -mouse_position;
     }
 }
