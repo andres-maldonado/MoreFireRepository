2c82454e25e834896c1c7562a3ab7d8d65557e8d
andres-maldonado <155512647+andres-maldonado@users.noreply.github.com>
2024-06-17 19:15:32 +0900

    Placeholder Minigame
    Added placeholder minigame to test the minigame system

---
+++ Assets/MinigameWin.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameWin : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Win()
+    {
+        GetComponent<Animator>().SetBool("isBeaten", true);
+        StartCoroutine(EndGame());
+    }
+    IEnumerator EndGame()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        Destroy(gameObject);
+        yield return null;
+    }
+}
