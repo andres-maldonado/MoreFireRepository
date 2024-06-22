2249c53ed8ba50e1ae7e04bdfd6459fa201dd7a0
omnairb <mobrian2004@gmail.com>
2024-06-22 13:26:03 +0900

    updating my branch

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,15 +7,6 @@ public class Item : ScriptableObject
     public string name;
     public string desc;
     public Sprite img;
-<<<<<<< HEAD
-    public void Init(string item_name, string item_desc, Sprite icon)
-    {
-        name = item_name;
-        desc = item_desc;
-        img = icon;
-    }
-    
-=======
 
     public void Init(string item_name, string item_desc, Sprite item_img)
     {
@@ -24,5 +15,4 @@ public class Item : ScriptableObject
         img = item_img;
     }
 
->>>>>>> 36201e6cf8a367d13e2f8a3d7998f5c9ce2cfdd5
 }
