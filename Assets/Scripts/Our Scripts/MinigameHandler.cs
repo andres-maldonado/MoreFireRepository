78d385013ffac85d9eefc676c059cf61ce53ece4
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-06-25 13:24:43 +0900

    Quests can be completed through minigames

--- Assets/Scripts/Our Scripts/MinigameHandler.cs
+++ Assets/Scripts/Our Scripts/MinigameHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MinigameHandler : MonoBehaviour
 {
+    [Tooltip("Comma-separated list of quest names to complete upon minigame win")]
+    [SerializeField] private string quests_to_complete;
     private bool minigame_ended = false;
     private Vector3 screen_bottom = new Vector3(0, -15, 0);
 
@@ -15,6 +17,9 @@ public class MinigameHandler : MonoBehaviour
 
     public void Win() { 
         AudioManager.instance.PlayOneShot(FMODEvents.instance.minigameWinSound, this.transform.position);
+        foreach (string q in quests_to_complete.Split(",")) {
+            QuestManager.Instance.CompleteQuest(q.Trim());
+        }
         if (on_win == end_behavior.destroy) Destroy(gameObject);
         else if (on_win == end_behavior.move_and_destroy) minigame_ended = true;
     }
