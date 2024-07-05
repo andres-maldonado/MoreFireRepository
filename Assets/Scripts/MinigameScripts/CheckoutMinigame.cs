305dae0c34d2c51a31bad6aa5c2a7e57a74550bd
omnairb <mobrian2004@gmail.com>
2024-07-05 16:44:27 +0900

    Checkout minigame screen and scanner

--- Assets/Scripts/MinigameScripts/CheckoutMinigame.cs
+++ Assets/Scripts/MinigameScripts/CheckoutMinigame.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using TMPro;
 
 public class CheckoutMinigame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_Text item_listUI;
+    [SerializeField] private List<string> checkout_screen = new List<string>();
+    [SerializeField] string[] items_checkout;
+    Dictionary<string, bool> checked_out = new Dictionary<string, bool>();
+    BoxCollider2D checkout_trigger;
+
+    void OnTriggerEnter2D(Collider2D other) 
     {
-        
+        if (checked_out.ContainsKey(other.name)) // checks if item is scanned
+        {
+            checked_out[other.name] = true;
+            checkout_screen.Add(other.name); 
+            update_checkout_screen();
+            Destroy(other.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void check_done()
     {
-        
+        bool done_scanning = true;
+        foreach(var item in checked_out)
+        {
+            if (item.Value == false)
+            {
+                done_scanning = false;
+            }
+        }
+        if (done_scanning)
+        {
+            print("done scanning !!");
+        }        
+    }
+
+    public void update_checkout_screen() // updtaes the text on the checkout screen
+    {
+        string receipt = "";
+        for (int i = 0; i < checkout_screen.Count; i++)
+        {
+            receipt += checkout_screen[i] + (i == checkout_screen.Count - 1 ? "" : "\n\n");
+        }
+        item_listUI.text = receipt;
+    }
+
+    void Start()
+    {
+        // initiates the map with all the items as not checked out
+        checkout_trigger = GetComponent<BoxCollider2D>();
+        for (int i = 0; i < items_checkout.Length; i++) 
+        {
+            checked_out.Add(items_checkout[i], false);
+        }
     }
 }
