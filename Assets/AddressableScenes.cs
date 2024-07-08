163f5a9ca39a0bd9ef744ccb9a766c7e07ffcff6
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-07-08 10:55:13 +0900

    Addressables works (mostly) with scenes now as well

--- Assets/AddressableScenes.cs
+++ Assets/AddressableScenes.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
 
 public class AddressableScenes : MonoBehaviour
 {
+
+    private string[] manager_names = new string[] {"GlobalManager", "AudioManager", "QuestManager", "GameSceneManager"};
+
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Init()
     {
-        
+        DontDestroyOnLoad(gameObject);
+        foreach (string m in manager_names) {
+            Debug.Log(m);
+            AsyncOperationHandle<GameObject> loadOp = Addressables.LoadAssetAsync<GameObject>(m);
+            yield return loadOp;
+
+            if (loadOp.Result != null) {
+                Instantiate(loadOp.Result, transform);
+            }
+        }
+
+        AsyncOperationHandle<SceneInstance> init_scene_op = Addressables.LoadSceneAsync("ShowcaseScene1", LoadSceneMode.Single);
+        yield return init_scene_op;
+
+        yield return new WaitForSeconds(2.5f);
+
+        Addressables.UnloadSceneAsync(init_scene_op);
+        init_scene_op = Addressables.LoadSceneAsync("ShowcaseScene2", LoadSceneMode.Single);
+        yield return init_scene_op;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Awake() {
+        StartCoroutine(Init());
     }
 }
