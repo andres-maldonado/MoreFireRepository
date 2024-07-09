d19984a29c88993c3d1e7147e19a5b2e84c05af8
omnairb <mobrian2004@gmail.com>
2024-07-09 14:25:06 +0900

    finished funcitonality for checkout mningame

--- Assets/Scripts/MinigameScripts/CheckoutMinigame.cs
+++ Assets/Scripts/MinigameScripts/CheckoutMinigame.cs
@@ -6,12 +6,15 @@ using TMPro;
 
 public class CheckoutMinigame : MonoBehaviour
 {
+    [SerializeField] BoxCollider2D collider;
     [SerializeField] Animator wallet;
     [SerializeField] private TMP_Text item_listUI;
     [SerializeField] private List<string> checkout_screen = new List<string>();
     [SerializeField] string[] items_checkout;
     Dictionary<string, bool> checked_out = new Dictionary<string, bool>();
     BoxCollider2D checkout_trigger;
+
+    bool finish_checkout = false;
 
     void OnTriggerEnter2D(Collider2D other) 
     {
@@ -20,13 +23,17 @@ public class CheckoutMinigame : MonoBehaviour
             checked_out[other.name] = true;
             checkout_screen.Add(other.name); 
             Debug.Log(other.name);
-            update_checkout_screen();
-            check_done();
+            if (!finish_checkout)
+            {
+                update_checkout_screen();
+                checkout_done();
+            }
             Destroy(other.gameObject);
+            done_pay();
         }
     }
 
-    void check_done()
+    void checkout_done()
     {
         bool done_scanning = true;
         foreach(var item in checked_out)
@@ -38,9 +45,28 @@ public class CheckoutMinigame : MonoBehaviour
         }
         if (done_scanning)
         {
-            Debug.Log("done scanning !!");
+            finish_checkout = true;
             wallet.Play("WalletEnter");
+            collider.size = new Vector2 (1.5f, 0.4f);
+            collider.offset = new Vector2 (-5, -3.4f);
+            checked_out.Add("Coin", false);
         }        
+    }
+
+    void done_pay()
+    {
+        bool paid = true;
+        foreach(var item in checked_out)
+        {
+            if (item.Value == false)
+            {
+                paid = false;
+            }
+        }
+        if (paid)
+        {
+            GetComponentInParent<MinigameWin>().Win();
+        }
     }
 
     public void update_checkout_screen() // updtaes the text on the checkout screen
@@ -55,6 +81,7 @@ public class CheckoutMinigame : MonoBehaviour
 
     void Start()
     {
+        collider = collider.GetComponent<BoxCollider2D>();
         // initiates the map with all the items as not checked out
         checkout_trigger = GetComponent<BoxCollider2D>();
         for (int i = 0; i < items_checkout.Length; i++) 
