59ca0e3a1b0dede3c7a44bf7b031f8846e932b39
andres-maldonado <155512647+andres-maldonado@users.noreply.github.com>
2024-07-22 16:56:25 +0900

    Minigame SFX Implemented

--- Assets/Scripts/MinigameScripts/PhoneMinigame.cs
+++ Assets/Scripts/MinigameScripts/PhoneMinigame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using FMODUnity;
 
 public class PhoneMinigame : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class PhoneMinigame : MonoBehaviour
     private string game_stage = "passcode";
     private string current_passcode = "";
     GameObject keypad;
+    [SerializeField] EventReference text;
+    [SerializeField] EventReference wrongPasscode;
+    [SerializeField] EventReference open;
 
     private void Start() {
         keypad = transform.Find("Keypad").gameObject;
@@ -26,12 +30,15 @@ public class PhoneMinigame : MonoBehaviour
 
     public void EnterKey(string n) {
         current_passcode += n;
+        AudioManager.instance.PlayOneShot(text, this.transform.position);
         if (current_passcode.Length == 4) {
             if (current_passcode != correct_passcode) {
                 current_passcode = "";
+                AudioManager.instance.PlayOneShot(wrongPasscode, this.transform.position);
             }
             else {
                 keypad.SetActive(false);
+                AudioManager.instance.PlayOneShot(open, this.transform.position);
                 transform.Find("AlertsButton").gameObject.SetActive(true);
                 transform.Find("Square").gameObject.SetActive(false);
             }
--- Assets/Scripts/Our Scripts/DragAndDrop.cs
+++ Assets/Scripts/Our Scripts/DragAndDrop.cs
@@ -1,21 +1,43 @@
 using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DragAndDrop : MonoBehaviour
 {
     [SerializeField] EventReference dragItem;
     [SerializeField] private bool flip_direction = true;
+    private bool hover = false;
     // moves the object to the mouse location and stops its velocity
     void OnMouseDrag() {
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 
         Vector3 mouse_position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x + Camera.main.transform.position.x, Input.mousePosition.y, Camera.main.transform.position.z + Camera.main.nearClipPlane));
         mouse_position.z = 0f;
-        AudioManager.instance.PlayOneShot(dragItem, this.transform.position);
         rigidbody.velocity = Vector2.zero;
         rigidbody.angularVelocity = 0f;
         transform.position = mouse_position * (flip_direction ? -1 : 1);
+        if (Input.GetMouseButtonDown(0))
+        {
+            AudioManager.instance.PlayOneShot(dragItem, this.transform.position);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        hover = true;
+    }
+    private void OnMouseExit()
+    {
+        hover = false;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && hover)
+        {
+            AudioManager.instance.PlayOneShot(dragItem, this.transform.position);
+        }
     }
 }
