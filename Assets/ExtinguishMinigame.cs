9c3e85551177156babb281d77d75eb59aacfbad4
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-07-24 18:21:43 +0900

    finalized (probably) extinguish minigame

--- Assets/ExtinguishMinigame.cs
+++ Assets/ExtinguishMinigame.cs
@@ -8,18 +8,27 @@ public class ExtinguishMinigame : MonoBehaviour
     private GameObject door_fire;
     private GameObject nozzle;
     private bool game_won = false;
-    
+    private ParticleSystem extinguisher_particles;
 
     void Start() {
         fires_parent = transform.Find("Fires").gameObject;
         nozzle = transform.Find("Extinguwuisher").gameObject;
         door_fire = GameObject.Find("/DoorFire");
+        extinguisher_particles = nozzle.GetComponent<ParticleSystem>();
+        extinguisher_particles.Stop();
     }
 
     void Update() {
         Vector3 mouse_position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x + Camera.main.transform.position.x, Input.mousePosition.y, Camera.main.transform.position.z + Camera.main.nearClipPlane));
         mouse_position.z = 0f;
         nozzle.transform.position = mouse_position;
+
+        if (Input.GetMouseButtonDown(0)) {
+            extinguisher_particles.Play();
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            extinguisher_particles.Stop();
+        }
 
         if (fires_parent.transform.childCount == 0 && !game_won) {
             GetComponentInParent<MinigameWin>().Win();
