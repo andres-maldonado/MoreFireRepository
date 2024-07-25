e41ebc1b04ecae91c5e4104ab2a864e8fcf34f88
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-07-25 09:28:36 +0900

    build tweaks

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -3,13 +3,13 @@ using UnityEditor;
 public class Builder
 {
     private static void BuildMac() {
-        BuildPipeline.BuildPlayer(new string[] {"Assets/Scenes/ShowcaseScene1.unity", "Assets/Scenes/ShowcaseScene2.unity"},
-                                    "MacBuilds/FireProto.app", BuildTarget.StandaloneOSX, BuildOptions.AutoRunPlayer);
+        BuildPipeline.BuildPlayer(new string[] {"Assets/Scenes/AddressableInitiator.unity"},
+                                    "MacBuilds/FireBuildFinal.app", BuildTarget.StandaloneOSX, BuildOptions.AutoRunPlayer);
     }
 
     private static void BuildWindows() {
-        BuildPipeline.BuildPlayer(new string[] {"Assets/Scenes/ShowcaseScene1.unity", "Assets/Scenes/ShowcaseScene2.unity"},
-                                    "WindowsBuilds/FireProto.exe", BuildTarget.StandaloneWindows, BuildOptions.AutoRunPlayer);
+        BuildPipeline.BuildPlayer(new string[] {"Assets/Scenes/AddressableInitiator.unity"},
+                                    "WindowsBuilds/FireBuildFinal.exe", BuildTarget.StandaloneWindows, BuildOptions.AutoRunPlayer);
     }
 
 }
