ce8663a81679cec254e1d2bb1b8d1954d391191d
andres-maldonado <155512647+andres-maldonado@users.noreply.github.com>
2025-07-02 18:29:32 -0400

    Password Fix and Error Fix

--- Assets/Scripts/MinigameScripts/Map_warning.cs
+++ Assets/Scripts/MinigameScripts/Map_warning.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,27 +9,27 @@ public class evac_warning : MonoBehaviour
     {
         if (this.GetComponentInParent<EvacuationMinigame>().step == 0)
         {
-            GlobalManager.Instance.DisplayError("There might be a more efficient route...", "Let's try finding a more direct path!");
+            GlobalManager.Instance.DisplayError("それは間違った方向です！", "他の方向に行ってみよう！");
         }
         else if (this.GetComponentInParent<EvacuationMinigame>().step == 1)
         {
-            GlobalManager.Instance.DisplayError("This doesn't seem to be the best way to go...", "Let's try finding a more direct path!");
+            GlobalManager.Instance.DisplayError("それは間違った方向です！", "他の方向に行ってみよう！");
         }
         else if (this.GetComponentInParent<EvacuationMinigame>().step == 2)
         {
-            GlobalManager.Instance.DisplayError("That's the wrong way!", "Let's try going the other way");
+            GlobalManager.Instance.DisplayError("それは間違った方向です！", "他の方向に行ってみよう！");
         }
         else if (this.GetComponentInParent<EvacuationMinigame>().step == 3)
         {
-            GlobalManager.Instance.DisplayError("This is not where I remember the shelter being...", "The shelter has to be somewhere around here...");
+            GlobalManager.Instance.DisplayError("それは間違った方向です！", "他の方向に行ってみよう！");
         }
         else if (this.GetComponentInParent<EvacuationMinigame>().step == 4)
         {
-            GlobalManager.Instance.DisplayError("This is not  I remember the shelter being...", "The shelter has to be somewhere around here...");
+            GlobalManager.Instance.DisplayError("それは間違った方向です！", "他の方向に行ってみよう！");
         }
         else if (this.GetComponentInParent<EvacuationMinigame>().step == 5)
         {
-            GlobalManager.Instance.DisplayError("Hmmm I'm pretty sure that I have gone this way...", "The shelter has to be somewhere around here...");
+            GlobalManager.Instance.DisplayError("それは間違った方向です！", "他の方向に行ってみよう！(E)");
         }
     }
 }
